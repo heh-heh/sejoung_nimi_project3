17d3d0b2b75412d283dd688580d5121061a40dc4
sung_won <81358337+heh-heh@users.noreply.github.com>
2023-09-07 09:43:14 +0900

    feat : 상점
    상점 추가
    상점 랜덤 으로 등장

--- Assets/scr/UI/forst_UI/around_frost.cs
+++ Assets/scr/UI/forst_UI/around_frost.cs
@@ -8,10 +8,14 @@ public class around_frost : MonoBehaviour
     // Start is called before the first frame update
     public GameObject sh_forst2_UI;
     public GameObject hid_forst_UI;
+    public List<GameObject> shop_UI = new List<GameObject>();
+    int random = 0;
     void Start()
     {
         sh_forst2_UI.SetActive(false);
         hid_forst_UI.SetActive(true);
+        shop_UI[0].SetActive(false);
+        shop_UI[1].SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,6 +25,9 @@ public class around_frost : MonoBehaviour
     }
 
     public void ch_UI(){
+        random = Random.Range(0,2);
+        shop_UI[random].SetActive(true);
+        Debug.Log(random);
         hid_forst_UI.SetActive(false);
         sh_forst2_UI.SetActive(true);
     }
